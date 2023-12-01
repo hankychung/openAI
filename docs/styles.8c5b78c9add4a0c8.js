15fec6dae507c3fe591613f486ac6952d1bdaf18
Han <zhonghanjie@flyele.net>
2023-12-01 17:08:15 +0800

    chore: format

--- docs/styles.8c5b78c9add4a0c8.js
+++ docs/styles.8c5b78c9add4a0c8.js
@@ -1,1 +1,9 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[532],{65632:()=>{}},s=>{var e;e=65632,s(s.s=e)}]);
+'use strict'
+;(self.webpackChunk = self.webpackChunk || []).push([
+  [532],
+  { 65632: () => {} },
+  (s) => {
+    var e
+    ;(e = 65632), s((s.s = e))
+  }
+])
